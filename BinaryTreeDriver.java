c38b9ca36dadefbdf7082fe8ea14da3c4a7d356d
Kyle <wong.kyle5@gmail.com>
2014-01-20 14:27:47 -0500

    Create BinaryTreeDriver.java

---
+++ BinaryTreeDriver.java
@@ -0,0 +1,45 @@
+package edu.jhu.cs.kwong23.ml.binarytree;
+
+import edu.jhu.cs.kwong23.ml.binarytree.BinaryTree.BinaryTreeNode;
+
+public class BinaryTreeDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree tree = new BinaryTree();
+		tree.insert(5);
+		tree.insert(3);
+		tree.insert(7);
+		tree.insert(2);
+		tree.insert(4);
+		tree.insert(6);
+		tree.insert(10);
+		tree.insert(8);
+//		tree.test();
+		tree.simpleprint();
+//		BinaryTree mytree = new BinaryTree();
+//		
+////		BinaryTreeNode root = new BinaryTreeNode();
+////		root.data = 5;
+//		BinaryTreeNode root = new BinaryTreeNode(5);
+//		
+//		BinaryTreeNode node2 = new BinaryTreeNode(2);
+//		BinaryTreeNode node3 = new BinaryTreeNode(3);
+//		BinaryTreeNode node4 = new BinaryTreeNode(4);
+//		
+//		root.left = node3;
+//		node3.left = node2;
+//		node3.right = node4;
+//		System.out.println(root.data);
+//		System.out.println(root.left.data);
+//		System.out.println(root.left.left.data);
+//		System.out.println(root.left.right.data);
+
+		
+		
+	}
+
+}
