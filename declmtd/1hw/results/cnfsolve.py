f5dd5aea463647a5f502f46b3223ab79a6386b0e
Kyle T Wong <kwong23@ugrad14.cs.jhu.edu>
2014-02-26 00:04:02 -0500

    resorted

---
+++ declmtd/1hw/results/cnfsolve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+## This program runs all .enc files through DIMACS and zchaff and outputs the formula and the resulting answer
+
+import os, subprocess
+
+def main():
+  for enc in sorted(os.listdir('.')):
+    basefn, ext = os.path.splitext(enc)
+    if enc.endswith('.enc'):# and (basefn + '.ans' not in os.listdir('.')):
+      cmd = 'convertToDIMACS {enc} > {basefn}.cnf'.format(**locals())
+      #print cmd
+      subprocess.call(cmd, shell=True)
+      cmd = 'zchaff {basefn}.cnf > {basefn}.output'.format(**locals())
+      #print cmd
+      subprocess.call(cmd, shell=True)
+      cmd = 'readOutput {basefn}.output {basefn}.key > {basefn}.ans'.format(**locals())
+      #print cmd
+      subprocess.call(cmd, shell=True)
+
+      cmd = 'cat {basefn}.enc'.format(**locals())
+      print cmd
+      subprocess.call(cmd, shell=True)
+      cmd = 'cat {basefn}.ans | sort'.format(**locals())
+      print cmd
+      subprocess.call(cmd, shell=True)
+      print
+
+## Function to call main
+if __name__ == '__main__':
+  main()
