2f294768819aa9e196c9817a8081dffe662166df
Kyle <wong.kyle5@gmail.com>
2014-02-26 00:55:57 -0500

    working dictionary word adds

--- declmtd/1hw/encoder.py
+++ declmtd/1hw/encoder.py
@@ -5,18 +5,45 @@ import os, subprocess
 #This script reads in a word dictionary and a puzzle file and outputs a CNF encoding to stdout
 
 def main():
-  dict_fn = ''
-  puzz_fn = ''
+  dict_fn = 'microwords'
+  puzz_fn = 'micro.puzzle'
   readDict(dict_fn)
   readPuzzle(puzz_fn)
 
 def readDict(dict_fn):
   dic = open(dict_fn, 'r')
-  #read and do work
+  # add # and all single letters to dictionary
+  word_dict = {}
+  word_dict['#'] = []
+  atoz = map(chr, range(97, 123)) 
+  for let in atoz:
+    word_dict['#'].append(let)
+    word_dict['#' + let] = []
+    word_dict['#' + let].append('#')
+
+  #go through dictionary and add the words
+  for word in dic:
+    word = word.strip() + '#'
+    for i in xrange(len(word)):
+      if ('#' + word[0:i]) in word_dict:
+        if word[i] not in word_dict['#' + word[0:i]]:
+          word_dict['#' + word[0:i]].append(word[i])
+      else:
+          word_dict['#' + word[0:i]] = []
+          word_dict['#' + word[0:i]].append(word[i])
+      #print word[0:i]
+      #print 'word[i] %s ' % word[i]
+      
+ 
+
+  for ans in word_dict['#o']:
+    print ans
 
 def readPuzzle(puzz_fn):
   puzz = open(puzz_fn, 'r')
+  print 'opened %s' % puzz_fn
   #read and do work
+
 
 ## Function to call main
 if __name__ == '__main__':
