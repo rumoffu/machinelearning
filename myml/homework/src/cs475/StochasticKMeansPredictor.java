c8d40ce9a5cf010b3af31601a201632caff062cf
Kyle <wong.kyle5@gmail.com>
2014-04-03 13:32:22 -0400

    added faster but wrong ska

--- myml/homework/src/cs475/StochasticKMeansPredictor.java
+++ myml/homework/src/cs475/StochasticKMeansPredictor.java
@@ -81,10 +81,12 @@ public class StochasticKMeansPredictor extends Predictor{
 			if(this.instanceToCluster[j] >= 0){ //was in an old cluster
 				rnk.get(this.instanceToCluster[j]).remove(new Integer(j));
 				updateCluster(this.instanceToCluster[j], instances);
+//				removeFromCluster(instances.get(j), j); //faster but might be wrong
 			}
 			this.instanceToCluster[j] = minCluster; //add to new min cluster
 			rnk.get(minCluster).add(j);
 			updateCluster(minCluster, instances);
+//			addToCluster(instances.get(minCluster), j); //faster but might be wrong
 		}
 		
 	}
@@ -103,6 +105,55 @@ public class StochasticKMeansPredictor extends Predictor{
 			sum = Util.scalarMultiply(1.0/cluster.size(), sum);
 		}
 		mewk.set(cluster_num, sum);
+	}
+	
+	/**
+	 * speed up 1 second insteaed of ~20 for ska but
+	Test (on train data from train model) VI: 2.496316
+	Test (on dev data from train model) VI: 2.506161
+
+	dev with mod VI: VI: 2.697404
+	train with mod VI: VI: 2.784189
+	 * @param instance
+	 * @param key_to_remove
+	 */
+	private void removeFromCluster(Instance instance, int key_to_remove){
+		ArrayList<Integer> cluster = rnk.get(this.instanceToCluster[key_to_remove]);
+		Double[] xi;
+		Double[] sum = mewk.get(this.instanceToCluster[key_to_remove]);
+		// use number in cluster to recalculate old sum
+		sum = Util.scalarMultiply(cluster.size(), sum);
+		xi = instance.getFeatureVector().getAll(this.number_of_features);
+		// remove this instance's contribution
+		xi = Util.scalarMultiply(-1, xi);
+		sum = Util.vectorAdd(sum, xi);
+		// remove key and update old mewk
+		cluster.remove(new Integer(key_to_remove));
+		if(cluster.size() > 0){
+			mewk.set(this.instanceToCluster[key_to_remove], Util.scalarMultiply(1.0/cluster.size(), sum));
+		}
+		else{ //empty 
+			sum = new Double[this.number_of_features];
+			for(int i = 0; i < this.number_of_features; i++){
+				sum[i] = 0.0;
+			}
+			mewk.set(this.instanceToCluster[key_to_remove], sum);
+		}
+	}
+	
+	private void addToCluster(Instance instance, int key_to_add){
+		ArrayList<Integer> cluster = rnk.get(this.instanceToCluster[key_to_add]);
+		Double[] xi;
+		Double[] sum = mewk.get(this.instanceToCluster[key_to_add]);
+		// use number in cluster to recalculate old sum
+		sum = Util.scalarMultiply(cluster.size(), sum);
+		xi = instance.getFeatureVector().getAll(this.number_of_features);
+		// add this instance's contribution
+		sum = Util.vectorAdd(sum, xi);
+		// add key and update new mewk
+		cluster.add(new Integer(key_to_add));
+		mewk.set(this.instanceToCluster[key_to_add], Util.scalarMultiply(1.0/cluster.size(), sum));
+		
 	}
 
 	/**
